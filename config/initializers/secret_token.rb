5ac18cb5344dae8e13a09461637e417357b3d8f8
Michał Krajewski <mkrajewski90@gmail.com>
2012-11-19 15:57:32 +0100

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GymSport::Application.config.secret_token = '2862ad13f0073fa5a5accb36cffd8b2a8c6ac983ad56ccaeb8b03ccb2cf45683a445054489c1159820e4c65653500a5d6cfd8736af8865824cdb26cb2e245c5d'
