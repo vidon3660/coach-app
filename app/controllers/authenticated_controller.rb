ddbae3b859375c3deb42e593b3b96a80e8c17b72
Michał Krajewski <mkrajewski90@gmail.com>
2012-12-04 23:28:00 +0100

    small ref

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,5 +1,12 @@
 class AuthenticatedController < ApplicationController
 	
   before_filter :authenticate_user!
+  before_filter :check_banned_user
+
+  private
+
+    def check_banned_user
+      redirect_to banned_path if current_user.banned?
+    end
 
 end
