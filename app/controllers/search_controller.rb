27627133c212eda16f412ce36ad6df60c7e32eba
Michał Krajewski <mkrajewski90@gmail.com>
2012-12-06 01:16:55 +0100

    change controller inheritance

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,4 @@
-class SearchController < ApplicationController
+class SearchController < AuthenticatedController
 
   def index
     @users = User.search params[:search], order: :first_name, conditions: { status: "active" }
