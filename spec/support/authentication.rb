56e12a8e74ca54e1418f57b8644e2d3233f906bc
Michał Krajewski <mkrajewski90@gmail.com>
2013-01-07 19:20:23 +0100

    integrate layout

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -4,8 +4,8 @@ def sign_in(user=nil)
   visit root_path
   click_link "Login"
   current_path.should == new_user_session_path
-  fill_in "Email", with: @user.email
-  fill_in "Password", with: @user.password
+  fill_in "user_email", with: @user.email
+  fill_in "user_password", with: @user.password
   click_button "Login"
   current_path.should == board_path
   page.has_content?("Signed in successfully.").should be_true
